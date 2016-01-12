3521ede7fdc770d076d2101b6f20a5426f2e8a46
Richard <richard.askin@gmail.com>
2016-01-12 16:19:34 +0100

    new1

--- js/scripts.js
+++ js/scripts.js
@@ -22,6 +22,27 @@ $(document).ready(function(){
 	    $( ".sortable" ).disableSelection();
 	});
 
+	// PREVENT SCROLL ON MOBILE DRAG
+	window.blockMenuHeaderScroll = false;
+	$(window).on('touchstart', function(e)
+	{
+	    if ($(e.target).closest('.sortable').length == 1)
+	    {
+	        blockMenuHeaderScroll = true;
+	    }
+	});
+	$(window).on('touchend', function()
+	{
+	    blockMenuHeaderScroll = false;
+	});
+	$(window).on('touchmove', function(e)
+	{
+	    if (blockMenuHeaderScroll)
+	    {
+	        e.preventDefault();
+	    }
+	});
+
 });
 
 var mobile;
